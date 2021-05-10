4e5f2d3331400b442e5bbe72d07fee7b2bf76d10
CarlottaQuensel <carlotta.quensel@web.de>
2021-05-10 11:22:01 +0200

    Added plot to analysis

--- data/Evaluation.py
+++ data/Evaluation.py
@@ -90,6 +90,9 @@ class evaluate():
                     label names for the classification probabilities
                     length must match the classifications of both output arguments
         """
+        if len(gold_output) != len(predicted_output):
+            raise IndexError("Unequal amount of gold and predicted labels. Use set_predictions to set again.")
+            return self
         try:
             self.labels = {i: label for i, label in enumerate(labels)}
         except NameError:
@@ -107,13 +110,75 @@ class evaluate():
             # Set the highest scoring label to 1
             prediction[max_prediction] = 1
             self.predicted.append(prediction)
+    
+    def set_predictions(self, gold_output, predicted_output, labels=None):
+        """Set gold and predicted labels for new data
+
+        Args:
+            gold_output (list(list(int))): 
+                    list of the gold classification labels used to evaluate the classifier
+                    each classification consists of 1 for the gold label and 0 for all others
+            predicted_output (list(list(float))): 
+                    list of the labels predicted by the classifier
+                    each classification consists of a list of probabilities for each label
+            labels (list(str)):
+                    label names for the classification probabilities
+                    length must match the classifications of both output arguments
+        """
+        try:
+            self.labels = {i: label for i, label in enumerate(labels)}
+        except NameError:
+            self.labels = {i: str(i) for i in range(len(gold_output[0]))}
+        self.gold = gold_output
+        # Save unaltered probabilities given by the classifier
+        self.predicted_raw = predicted_output
+
+        # For comparison with the gold labels, convert the probabilities into 1 for the most likely, and 0 for all other labels
+        self.predicted = []
+        for classification in self.predicted_raw:
+            # Get index of highest probability score
+            max_prediction = classification.index(max(classification))
+            prediction = [0 for i in range(len(self.labels))]
+            # Set the highest scoring label to 1
+            prediction[max_prediction] = 1
+            self.predicted.append(prediction)
+        self.reset_errors()
+
+    def reset_errors(self):
+        """After swapping the predictions, the errors should be reset to 0
+        """
+        self.errors = {label_index: {"tp": 0, "tn": 0, "fp": 0, "fn": 0} for label_index in self.labels}
+
         
-    def precision(label=None):
+    def count_errors(self):
+        """Count correct and false labels as 
+            TP - true positives are the correct labels
+            TN - true negatives are correctly unlabelled instances
+            FP - false positives are falsely labelled instances
+            FN - false negatives are falsely not labelled
+        """
+        self.errors = {label_index: {"tp": 0, "tn": 0, "fp": 0, "fn": 0} for label_index in self.labels}
+        for i in range(len(self.gold)):
+            gold = self.gold.index(1)
+            pred = self.predicted.index(1)
+            if gold == pred:
+                self.errors[gold]["tp"] += 1
+            else:
+                self.errors[gold]["fn"] += 1
+                self.errors[pred]["fp"] += 1
+         true_negs = set(self.labels).difference({gold,pred})
+         for label in true_negs:
+             self.errors[label]["tn"] += 1
+
+
+    def precision(self, label=None):
         """compute precision for one or all labels
 
         Args:
             label (string, optional): If set, only the precision score for this label is computed. Defaults to None.
         """
-        #if label:
+        if label:
             # find label in the label dictionary and compute precision for the respective class
-            # else compute precision for all classes
+        else:
+            # compute precision for all classes
+                        
