5bb8e3f3edfa3cc4f93d482ec132726ab01aa242
Kati1995Smi <katrin.schmidt1995@gmx.de>
2021-05-14 15:37:17 +0200

    Updated the code

--- data/poetryfoundation_tokenizer.py
+++ data/poetryfoundation_tokenizer.py
@@ -6,10 +6,17 @@ import pandas
 
 with open('/Users/katrin/Desktop/Master/Team Lab/Author_Classification-Team_Lab-1/data/poetryfoundation-dataset.csv', encoding="utf-8") as f:
     data = pandas.read_csv(f)
-    data_dict = data.set_index('Author').T.to_dict('list')
-    first2pairs = {k: data_dict[k] for k in list(data_dict)[:2]}
-    print(first2pairs)
-    
+    dictionary_of_poems = data.set_index('Author')['Content'].to_dict()
+    list_of_poems = []
+    for Author in dictionary_of_poems:
+        poem = dictionary_of_poems.get(Author)
+        list_of_poems.append([poem]) # list_of_poems = []  #[[poem1], [poem2], [poem3], ...]
+    #first2pairs = {k: dictionary_of_poems[k] for k in list(dictionary_of_poems)[:5]}
+    print()
+
+['string....']
+{Wendy Videlock: [['poem...'],[poem2],[...] ....}
     #tokenize_punctuation = WordPunctTokenizer()
     #tokens = tokenize_punctuation.tokenize()
+    #dictionary_of_poems = data.set_index('Author').T.to_dict('list')
         
