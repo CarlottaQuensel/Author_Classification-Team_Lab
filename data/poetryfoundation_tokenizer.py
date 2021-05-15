aaec4547f08d152fffddad496ba46e40e1ec20ce
Kati1995Smi <katrin.schmidt1995@gmx.de>
2021-05-15 20:47:39 +0200

    Finished tokenization

--- data/poetryfoundation_tokenizer.py
+++ data/poetryfoundation_tokenizer.py
@@ -1,6 +1,7 @@
 import nltk
 from nltk.tokenize import WordPunctTokenizer
 import pandas
+# TODO: think about replacing '\n' by ' .' --> if ' .' is followed by '.'
 
 
 # open csv file with pandas
@@ -15,16 +16,20 @@ with open('/Users/katrin/Desktop/Master/Team Lab/Author_Classification-Team_Lab-
     # so that dictionary_of_poems = {'Author': ['poem1', 'poem2',...]}
     for index, row in data_frame.iterrows():
         if row[1] not in dictionary_of_poems:
-            dictionary_of_poems[row[1]] = [str([row[4]]).replace('\n', ' ')] #[str([row[4]]).strip('\n')]
+            dictionary_of_poems[row[1]] = [str(row[4]).replace('\n', ' ')] #[str([row[4]]).strip('\n')]
         else:
             dictionary_of_poems[row[1]].append(str(row[4]).replace('\n', ' '))
 
     # create a smaller data_frame for testing
-    data_frame_head = {k: dictionary_of_poems[k] for k in list(dictionary_of_poems)[:5]}
-    print(data_frame_head)
+    # data_frame_head = {k: dictionary_of_poems[k] for k in list(dictionary_of_poems)[:1]}
 
-
-    #tokenize_punctuation = WordPunctTokenizer()
-
-    #tokens = tokenize_punctuation.tokenize()
- 
+    tokenize_punctuation = WordPunctTokenizer()
+    tokenized_dictionary = {}
+    for Author in dictionary_of_poems:
+        for poem in dictionary_of_poems[Author]:
+            tokenized_poem = tokenize_punctuation.tokenize(poem)
+            if Author not in tokenized_dictionary:
+                tokenized_dictionary[Author] = [tokenized_poem]
+            else:
+                tokenized_dictionary[Author].append(tokenized_poem)
+    print(tokenized_dictionary)
