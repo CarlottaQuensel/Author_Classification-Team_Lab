a9f6a9ee41f00731653246e6112eab7295b6d6cf
Kati1995Smi <katrin.schmidt1995@gmx.de>
2021-05-15 16:04:40 +0200

    Conversion from csv to dict() works now

--- data/poetryfoundation_tokenizer.py
+++ data/poetryfoundation_tokenizer.py
@@ -3,20 +3,27 @@ from nltk.tokenize import WordPunctTokenizer
 import pandas
 
 
+# open csv file with pandas
+with open('/Users/katrin/Desktop/Master/Team Lab/Author_Classification-Team_Lab-1/data/poetryfoundation-dataset.csv', encoding="utf-8") as file:
+    data_frame = pandas.read_csv(file)
 
-with open('/Users/katrin/Desktop/Master/Team Lab/Author_Classification-Team_Lab-1/data/poetryfoundation-dataset.csv', encoding="utf-8") as f:
-    data = pandas.read_csv(f)
-    dictionary_of_poems = data.set_index('Author')['Content'].to_dict()
-    list_of_poems = []
-    for Author in dictionary_of_poems:
-        poem = dictionary_of_poems.get(Author)
-        list_of_poems.append([poem]) # list_of_poems = []  #[[poem1], [poem2], [poem3], ...]
-    #first2pairs = {k: dictionary_of_poems[k] for k in list(dictionary_of_poems)[:5]}
-    print()
+    # initiate an empty dictionary
+    dictionary_of_poems = {}
 
-['string....']
-{Wendy Videlock: [['poem...'],[poem2],[...] ....}
+    # iterate over rows in data_frame,
+    # add 'Author' and 'Content' to the dict,
+    # so that dictionary_of_poems = {'Author': ['poem1', 'poem2',...]}
+    for index, row in data_frame.iterrows():
+        if row[1] not in dictionary_of_poems:
+            dictionary_of_poems[row[1]] = [row[4]]
+        else:
+            dictionary_of_poems[row[1]].append(row[4])
+
+    # create a smaller data_frame for testing
+    data_frame_head = {k: dictionary_of_poems[k] for k in list(dictionary_of_poems)[:5]}
+    print(data_frame_head)
+
+
     #tokenize_punctuation = WordPunctTokenizer()
     #tokens = tokenize_punctuation.tokenize()
-    #dictionary_of_poems = data.set_index('Author').T.to_dict('list')
-        
+ 
