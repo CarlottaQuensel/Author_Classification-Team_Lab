1b4dff002b97ccd4484d6df3f88fd95fd6c8a3a3
Kati1995Smi <katrin.schmidt1995@gmx.de>
2021-06-01 09:04:43 +0200

    Added folder to import modules

--- baseline/main.py
+++ baseline/main.py
@@ -1,11 +1,12 @@
 #from train import MaxEnt
 #from learnFeatures import learnFeatures
-from poetryfoundation_tokenizer import set_to_vec
-from train import MaxEnt
+from data.poetryfoundation_tokenizer import set_to_vec
+from baseline.train import MaxEnt
 import pickle
+
 # Set to either Kati's or Carlotta's file path
-# path = "/Users/katrin/Desktop/Master/Team Lab/Author_Classification-Team_Lab-1/data/"
-path = 'C:/Users/HP Envy/Documents/Uni/Master/SS21/topics in emotion analysis/Author_Classification-Team_Lab/data/'
+#path = "/Users/katrin/Desktop/Master/Team Lab/Author_Classification-Team_Lab-1/data/"
+#path = 'C:/Users/HP Envy/Documents/Uni/Master/SS21/topics in emotion analysis/Author_Classification-Team_Lab/data/'
 
 data = pickle.load(open(f'{path}data.pickle', 'rb'))
 vocab = pickle.load(open(f'{path}vocabulary.pickle', 'rb'))
