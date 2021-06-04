6d4775e2f260435955e4eb9eb46f7647099ac95e
CarlottaQuensel <carlotta.quensel@web.de>
2021-06-04 21:26:00 +0200

    added trace to training and vocab to feature learning

--- baseline/train.py
+++ baseline/train.py
@@ -22,7 +22,9 @@ class MaxEnt():
     features = list()
 
     def __init__(self, data: list[tuple[tuple[int], str]]=None, class_features: int=None) -> None:
-        """Initializing an instance of a Maximum Entropy classifier, if data is already given, 
+        """
+        Author: Katrin Schmidt
+        Initializing an instance of a Maximum Entropy classifier, if data is already given, 
         then also learning class features
 
         Args:
@@ -35,8 +37,10 @@ class MaxEnt():
             else:
                 self.learnFeatures(data)
 
-    def learnFeatures(self, data: list[tuple[tuple[int], str]], class_features: int=50) -> None:
-        """Compute the best features for the classifier based on pointwise mutual information between classes and document features
+    def learnFeatures(self, data: list[tuple[tuple[int], str]], class_features: int=50, vocabulary: dict[str] = None) -> None:
+        """
+        Author: Carlotta Quensel
+        Compute the best features for the classifier based on pointwise mutual information between classes and document features
         in a dataset and save them in a list with a matching list of (untrained) weights. The features are saved as functions for 
         the Max Ent classification and have an associated label and a document property as a number, e.g.
             -> label="Shakespeare" and property=45 (index of "Thou", is in the document).
@@ -44,7 +48,11 @@ class MaxEnt():
         Args:
             data (list[tuple[tuple[int], str]]): Dataset consisting of a list of document-label pairs, where the documents are word vectors
             class_features (int): The maximum number of features learned per class, default 50
+            vocabulary (dict[str]): The assignment of words to word vector indices used in the given data set
         """
+        if vocabulary:
+            self.vocabulary = vocabulary
+
         # Learning the feature-functions uses PMI and is explained more thoroughly in learnFeatures.py
         self.features = learnFeatures(data, class_features)
 
@@ -58,7 +66,9 @@ class MaxEnt():
         print(f"The classifier learned {len(self.features)} features for {len(self.labels)} classes.")
     
     def classify(self, document: list[int], in_training: str=False, weights: list[int]=None):
-        """The classifier predicts the most probable label from its label set for a document given as a word vector. The Maximum Entropy classifier works
+        """
+        Author: Carlotta Quensel
+        The classifier predicts the most probable label from its label set for a document given as a word vector. The Maximum Entropy classifier works
         with a function of a document and label, returning either 1 if if applies or 0 if it doesn't apply to the document-label pair and the function's
         correspinding weight. The probability of a label y given a document is the exponential function of the sum of all weights with applied functions 
         divided (normalized) by the sum of this exponential for all possible labels: p(y|doc) = exp(Σᵢ wᵢ·fᵢ(y,doc)) / Σ(y') exp(Σᵢ wᵢ·fᵢ(y',doc)).
@@ -82,7 +92,6 @@ class MaxEnt():
             print(f"The classifier needs exactly one weight per function. You used {len(weights)} weights for {len(self.features)} functions.")
             return None
 
-        # When testing the a
         # The numerator of the Max Ent-probability is the exponentioal function of every weight*function with the current label and given document
         numerator = dict()
         for label in self.labels:
@@ -93,6 +102,7 @@ class MaxEnt():
         p = dict()
         for label in self.labels:
             p[label] = numerator[label] / denominator
+            print(label, p[label])
         # The classifier either returns the most probable label or in training returns the label's probability
         if in_training:
             return p[in_training]
@@ -100,7 +110,9 @@ class MaxEnt():
 
 
     def accuracy(self, data: list[tuple[tuple[int], str]], weights: list[int]=None) -> float:
-        """Compute the basic accuracy of the classifier with a specific weight set as the percentage of correct predictions
+        """
+        Author: Carlotta Quensel
+        Compute the basic accuracy of the classifier with a specific weight set as the percentage of correct predictions
         from the overall number of predictions
 
         Args:
@@ -123,8 +135,9 @@ class MaxEnt():
         return tp/len(gold)
 
 
-    def train(self, data: list[tuple[tuple[int], str]], min_improvement: float=0.001):
+    def train(self, data: list[tuple[tuple[int], str]], min_improvement: float = 0.001, trace: bool = False):
         ''' 
+        Author: Katrin Schmidt, trace and accuracy Carlotta Quensel
         Method that trains the model via multivariable linear optimization.
         Since the optimization for the lambda vector needs to happen simultaneous, 
         the iterations stop after it counts 100 (instead of a specific value).
@@ -132,6 +145,8 @@ class MaxEnt():
         Args: 
             data (list[tuple[tuple[int], str]]): 
                 Dataset as a list of document vector-label pairs.
+            min_improvement (float, optional): The minimum accuracy improvement needed for a new iteration of optimization. Defaults to 0.0001
+            trace (boolean, optional): Switch on to track the development of the accuracy during the iterative process.
         '''
         # Training as an optimization process for the weights
         # Either with fixed number of iterations or until the improvement drops below a threshold (currently at least 0.1% improvement)
@@ -139,28 +154,39 @@ class MaxEnt():
         # compute old accuracy with random weights
         old_accuracy = 0
         new_accuracy = self.accuracy(data)
+        if trace:
+            print(f"With randomized weights for the features, the classifier's accuracy is {new_accuracy}.")
+            acc = [new_accuracy]
         new_lambda = list()
 
+        if trace:
+            i = 0
         while new_accuracy - old_accuracy >= min_improvement:
         #total_iterations = 100
         #for n in range(total_iterations):
             # Don't reassign the new weights in the first step (no new weights yet)
             if len(new_lambda):
                 self.weights = new_lambda
+                if trace:
+                    print(f"In the {i}. iteration, the classifier's accuracy improved by {new_accuracy - old_accuracy} and is now at {new_accuracy}.")
             # Perform stochastic gradient descent by iterating over features and computing the partial_derivative
             # and save the updated weights temporarily to first make sure that they improve the classifier
             new_lambda = [self.weights[i] - self.partial_derivative(data, i) for i in range(len(self.features))]
 
             # Calculate the accuracy with the new weights to check its improvement
             new_accuracy = self.accuracy(data, new_lambda)
+            if trace:
+                acc.append(new_accuracy)
 
-        # calculate the residual to check if the optimization works    
-        residual = [x1 - x2 for (x1, x2) in zip(new_lambda, self.weights)]
-        return self.weights
+        if trace:
+            print(f"The training consisted of {i} optimization steps in which the accuracy improved from {acc[0]} to {acc[-1]}.")
+            # If the user wants to track the training process, the accuracy scores are returned to potentially plot the improvement
+            return acc
 
   
     def partial_derivative(self, data: list[tuple[tuple[int],str]], i: int) -> float:
         '''
+        Author: Katrin Schmidt
         Method that computes the derivative of the objective function F
         by substracting the derivative of A from the derivative of B.
 
