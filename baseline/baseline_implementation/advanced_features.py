6cc39d0a28ba9f2773b304f3c7e90f1f9b3ec0e6
CarlottaQuensel <carlotta.quensel@web.de>
2021-07-18 15:51:54 +0200

    Features class

--- baseline/baseline_implementation/advanced_features.py
+++ baseline/baseline_implementation/advanced_features.py
@@ -3,6 +3,15 @@ import pronouncing
 from nltk import WordPunctTokenizer
 
 
+class Poem:
+	def __init__(self, poem_string):
+		self.string = poem_string
+	
+	def getFeatures(self):
+		self.rhyme = getRhymes(self.string)
+		
+
+
 def getRhymes(poem):
 	tok = WordPunctTokenizer()
 	stanzas = poem.splitlines()
@@ -20,18 +29,17 @@ def getRhymes(poem):
 			i += 1
 	rhymes = ["a", "b", "c", "d"]
 	verses = {i for i in range(len(stanzas))}
-	bins = []
+	bins = {}
 	for i in range(max(len(stanzas), 4)):
 		if i in verses:
 			verses.remove(i)
 			schema = rhymes.pop(0)
-			bins.append((i, schema))
+			bins[i] = schema
 			for j in range(i+1,len(stanzas)):
 				if stanzas[j][-1] in pronouncing.rhymes(stanzas[i][-1]) or stanzas[i][-1] == stanzas[j][-1]:
-					bins.append((j,schema))
+					bins[j] = schema
 					verses.remove(j)
-	bins.sort()
-	rhyme_scheme = bins[0][1]+bins[1][1]+bins[2][1]+bins[3][1]
+	rhyme_scheme = bins[0]+bins[1]+bins[2]+bins[3]
 	return rhyme_scheme
 
 def getBagOfWords(poem):
