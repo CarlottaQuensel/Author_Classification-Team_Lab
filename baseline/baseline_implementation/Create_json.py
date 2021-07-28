ea41eb3651c0f92373c7c80c8e9721877d855344
Kati1995Smi <katrin.schmidt1995@gmx.de>
2021-07-28 22:30:57 +0200

    Added poems (untokenized, json)

---
+++ baseline/baseline_implementation/Create_json.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Author: Katrin Schmidt
+import pandas
+import json
+
+
+path = "/Users/katrin/Desktop/Master/Team_Lab/Author_Classification/Author_Classification-Team_Lab/data/"
+
+# open csv file with pandas
+with open(f'{path}poetryfoundation-dataset.csv', encoding="utf-8") as file:
+    data_frame = pandas.read_csv(file)
+
+    # initiate an empty dictionary
+    dictionary_of_poems = {}
+
+    # iterate over rows in data_frame,
+    # add 'Author' and 'Content' to the dict,
+    # so that dictionary_of_poems = {'Author': ['poem1', 'poem2',...]}
+    for index, row in data_frame.iterrows():
+        if row[1] not in dictionary_of_poems:
+            dictionary_of_poems[row[1]] = [str(row[4]).replace('\n', ' ')]
+        else:
+            dictionary_of_poems[row[1]].append(str(row[4]).replace('\n', ' '))
+
+
+json_object = json.dumps(dictionary_of_poems)
+jsonFile = open("poems.json", "w")
+jsonFile.write(json_object)
+jsonFile.close()
+
+#print(json_object) 
